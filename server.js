45ac661a563614092f5fb11da990bc75e89af930
Matti Aho <ookooko@gmail.com>
2018-03-02 09:39:15 +0200

    pientä viritystä

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 
 let app = express();
-let port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.send('<p>Testi</p>');
-})
+});
 
 app.listen(port, () => {
   console.log(`Server is up in port ${port}`)
-})
+});
